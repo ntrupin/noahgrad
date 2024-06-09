bd823b56eaa4fb430c123598c1d39985a76abec0
ntrupin <natrupin@gmail.com>
2024-06-08 20:23:21 -0400

    adam

--- noahgrad/optim.py
+++ noahgrad/optim.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import List
+from typing import List, Tuple
 
 from noahgrad.core import Tensor
 
@@ -22,3 +22,24 @@ class SGD(Optimizer):
     def step(self):
         for p in self.parameters:
             p.data -= p.grad * self.lr
+
+class Adam(Optimizer):
+    def __init__(self, parameters: List[Tensor], learning_rate: float = 1e-3,
+                 betas: Tuple[float, float] = (0.9, 0.999), eps: float = 1e-8):
+        super().__init__(parameters, learning_rate)
+        self.betas = betas
+        self.eps = eps
+        self.t = 0
+        self.m = [np.zeros_like(p.data) for p in parameters]
+        self.v = [np.zeros_like(p.data) for p in parameters]
+
+    def step(self):
+        self.t += 1
+        for (i, p) in enumerate(self.parameters):
+            self.m[i] = self.betas[0] * self.m[i] + \
+                    (1 - self.betas[0]) * p.grad
+            self.v[i] = self.betas[1] * self.v[i] + \
+                    (1 - self.betas[1]) * p.grad
+            mhat = self.m[i] / (1 - self.betas[0] ** self.t)
+            vhat = self.v[i] / (1 - self.betas[1] ** self.t)
+            p.data -= self.lr * (mhat / (np.sqrt(vhat) + self.eps))
